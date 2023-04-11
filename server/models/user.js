92773e084ff69293db790ee94d52a404ab604adc
Bernardo Semião <berna.1991.1@gmail.com>
2023-04-11 21:28:26 +0200

    replaced bcrypt for bcrypt

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcryptjs = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
@@ -10,13 +10,13 @@ const userSchema = new mongoose.Schema({
 userSchema.pre('save', async function (next) {
     const user = this;
     if (user.isModified('password')) {
-        user.password = await bcrypt.hash(user.password, 10);
+        user.password = await bcryptjs.hash(user.password, 10);
     }
     next();
 });
 
 userSchema.methods.comparePassword = async function (candidatePassword) {
-    return await bcrypt.compare(candidatePassword, this.password);
+    return await bcryptjs.compare(candidatePassword, this.password);
 };
 
 const User = mongoose.model('User', userSchema);
